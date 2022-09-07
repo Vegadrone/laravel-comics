dea8e7663f1f0e879c3781a5532f4dd884d4a596
Vegadrone <spenaantonino@gmail.com>
2022-09-07 18:32:33 +0200

    1 card try

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
                 <div class="col-12 wrapper d-flex flex-wrap">
                     <div class="flag text-uppercase fw-bold text-light fs-3">current series</div>
                     <div class="comic-card">
-                        <img :src="cover" :alt="series" />
-                        <h5 class="fw-bold text-light mt-3 fs-6"></h5>
+                        <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eXhttps://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="series" />
+                        <h5 class="fw-bold text-light mt-3 fs-6">cicco</h5>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
                         <button class="btn btn-primary fw-bold">Load More</button>
@@ -23,7 +23,7 @@
                     <div class="row d-flex">
                         <div class="col d-flex align-items-center justify-content-center" v-for="(img, index) in imgList"
                             :key="index">
-                            <img :src="require(`../assets/img/buy-${img.imgPath}`)" alt="img.alt">
+                            <img :src="require(`../assets/images/buy-${img.imgPath}`)" alt="img.alt">
                             <h6 class="text-light text-uppercase"></h6>
                         </div>
                     </div>
