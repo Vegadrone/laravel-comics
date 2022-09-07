523a2d0cfa565680df912f316e1852ed59c63a6b
Vegadrone <spenaantonino@gmail.com>
2022-09-07 16:21:11 +0200

    testing routes and yield

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <h1>CONTENUTO</h1>
+@endsection
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DC Comics |</title>
+</head>
+<body>
+    <header>
+        <h3>header</h3>
+    </header>
+    <main>
+        @yield('main-content')
+    </main>
+    <footer>
+        <h3>footer</h3>
+    </footer>
+</body>
+</html>
