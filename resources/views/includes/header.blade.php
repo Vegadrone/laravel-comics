0fb6c2abd2970b84be9fec9a4f023111eb490fb1
Vegadrone <spenaantonino@gmail.com>
2022-09-07 17:09:33 +0200

    install and import bootstrap

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,12 @@
   <header>
-        <h3>header llalalalal</h3>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis nam sunt fugiat adipisci possimus! Ut, officia reprehenderit illo asperiores possimus quisquam error blanditiis eius et. Laboriosam explicabo eaque eius accusamus?
+                </div>
+                <div class="col-6">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, harum corrupti. Deleniti veritatis ducimus, rerum nostrum quisquam quis soluta, exercitationem itaque odio, libero omnis quasi ipsam! Praesentium quis quos velit.
+                </div>
+            </div>
+        </div>
     </header>
