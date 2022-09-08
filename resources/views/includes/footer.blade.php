18198d24ad75e99e23966e39629d95a600729d08
Vegadrone <spenaantonino@gmail.com>
2022-09-08 09:23:48 +0200

    fix backgrounds in footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,66 +1,68 @@
  <footer>
-     <div class="container">
-         <div class="row">
-             <div class="col-6">
-                 <div class="row">
-                     <div class="col-4">
-                         <div class="menu-voice">
-                             <h4>Dc comics</h4>
-                             <ul>
-                                 <li><a href="#">Characters</a></li>
-                                 <li><a href="#">Comics</a></li>
-                                 <li><a href="#">Movies</a></li>
-                                 <li><a href="#">Tv</a></li>
-                                 <li><a href="#">Games</a></li>
-                                 <li><a href="#">Collectibles</a></li>
-                                 <li><a href="#">Videos</a></li>
-                                 <li><a href="#">Fans</a></li>
-                                 <li><a href="#">News</a></li>
-                                 <li><a href="#">Shop</a></li>
-                             </ul>
+     <div class="upper-footer">
+         <div class="container">
+             <div class="row">
+                 <div class="col-6">
+                     <div class="row">
+                         <div class="col-4">
+                             <div class="menu-voice">
+                                 <h4>Dc comics</h4>
+                                 <ul>
+                                     <li><a href="#">Characters</a></li>
+                                     <li><a href="#">Comics</a></li>
+                                     <li><a href="#">Movies</a></li>
+                                     <li><a href="#">Tv</a></li>
+                                     <li><a href="#">Games</a></li>
+                                     <li><a href="#">Collectibles</a></li>
+                                     <li><a href="#">Videos</a></li>
+                                     <li><a href="#">Fans</a></li>
+                                     <li><a href="#">News</a></li>
+                                     <li><a href="#">Shop</a></li>
+                                 </ul>
+                             </div>
+                             <div class="menu-voice">
+                                 <h4>Shop</h4>
+                                 <ul>
+                                     <li><a href="#">Shop DC</a></li>
+                                     <li><a href="#">Shop DC Collectibles</a></li>
+                                 </ul>
+                             </div>
                          </div>
-                         <div class="menu-voice">
-                             <h4>Shop</h4>
-                             <ul>
-                                 <li><a href="#">Shop DC</a></li>
-                                 <li><a href="#">Shop DC Collectibles</a></li>
-                             </ul>
+                         <div class="col-4">
+                             <div class="menu-voice">
+                                 <h4>Dc</h4>
+                                 <ul>
+                                     <li><a href="#">Term Of Use</a></li>
+                                     <li><a href="#">Privacy Policy(New)</a></li>
+                                     <li><a href="#">Ad Choices</a></li>
+                                     <li><a href="#">Advertising</a></li>
+                                     <li><a href="#">Jobs</a></li>
+                                     <li><a href="#">Subscriptions</a></li>
+                                     <li><a href="#">Talent Workshops</a></li>
+                                     <li><a href="#">CPSC Certificate</a> s</li>
+                                     <li><a href="#">Ratings</a></li>
+                                     <li><a href="#">Shop Help</a></li>
+                                     <li><a href="#">Contact Us</a></li>
+                                 </ul>
+                             </div>
                          </div>
-                     </div>
-                     <div class="col-4">
-                         <div class="menu-voice">
-                             <h4>Dc</h4>
-                             <ul>
-                                 <li><a href="#">Term Of Use</a></li>
-                                 <li><a href="#">Privacy Policy(New)</a></li>
-                                 <li><a href="#">Ad Choices</a></li>
-                                 <li><a href="#">Advertising</a></li>
-                                 <li><a href="#">Jobs</a></li>
-                                 <li><a href="#">Subscriptions</a></li>
-                                 <li><a href="#">Talent Workshops</a></li>
-                                 <li><a href="#">CPSC Certificate</a> s</li>
-                                 <li><a href="#">Ratings</a></li>
-                                 <li><a href="#">Shop Help</a></li>
-                                 <li><a href="#">Contact Us</a></li>
-                             </ul>
-                         </div>
-                     </div>
-                     <div class="col-4">
-                         <div class="menu-voice">
-                             <h4>Sites</h4>
-                             <ul>
-                                 <li><a href="#">DC</a></li>
-                                 <li><a href="#">MAD Magazine</a></li>
-                                 <li><a href="#">DC Kids</a></li>
-                                 <li><a href="#">DC Univers</a></li>
-                                 <li><a href="#">DC Power Visa</a></li>
-                             </ul>
+                         <div class="col-4">
+                             <div class="menu-voice">
+                                 <h4>Sites</h4>
+                                 <ul>
+                                     <li><a href="#">DC</a></li>
+                                     <li><a href="#">MAD Magazine</a></li>
+                                     <li><a href="#">DC Kids</a></li>
+                                     <li><a href="#">DC Univers</a></li>
+                                     <li><a href="#">DC Power Visa</a></li>
+                                 </ul>
+                             </div>
                          </div>
                      </div>
                  </div>
-             </div>
-             <div class="col-6">
-                 <img src="../assets/img/dc-logo-bg.png" alt="logo-dc" />
+                 <div class="col-6">
+                     <img src="../assets/images/dc-logo-bg.png" alt="logo-dc" />
+                 </div>
              </div>
          </div>
      </div>
